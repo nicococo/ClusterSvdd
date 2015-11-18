1b61cf75bf24169d56264467b3efdbf44c773a06
nicococo <sinnl0s>
2015-11-18 15:31:10 +0100

    refactoring of tests anom+robust

--- test_anomdetect.py
+++ test_anomdetect.py
@@ -1,4 +1,6 @@
 __author__ = 'Nico Goernitz'
+
+
 import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
 import numpy as np
@@ -11,7 +13,6 @@ from cluster_svdd import ClusterSvdd
 def generate_data(datapoints, outlier_frac=0.1, dims=2):
     X = np.zeros((dims, datapoints))
     y = np.zeros(datapoints)
-
     num_noise = np.floor(datapoints*outlier_frac)
     num_dpc = np.floor(float(datapoints-num_noise)/2.0)
 
@@ -32,61 +33,43 @@ def generate_data(datapoints, outlier_frac=0.1, dims=2):
     return X, y
 
 
-if __name__ == '__main__':
-    nus = [0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2]
-    sigmas = [0.1, 0.25, 0.5, 1.0, 2.0]
-    ks = [1, 2, 3, 4]
+def plot_results(fname):
+    foo = np.load(fname)
+    maucs = foo['maucs']
+    saucs = foo['saucs']
+    nus = foo['nus']
+    ks = foo['ks']
 
-    reps = 20  # number of repetitions for performance measures
-    ntrain = 1000  # total number of data points is ntrain+ntest
-    ntest = 2000   # nval is part of ntrain
-    nval = 400
-    plot = True
+    plt.figure(1)
+    cols = np.random.rand(maucs.shape[1], 3)
+    fmts = ['-x', '--o', '--D', '--s', '--H']
+    for i in range(maucs.shape[1]):
+        plt.errorbar(nus, maucs[:, i], saucs[:, i], fmt=fmts[i], color=cols[i, :], ecolor=cols[i, :], linewidth=2.0, elinewidth=1.0, alpha=0.8)
+    plt.xlim((-0.0, 0.21))
+    plt.ylim((0.35, 1.05))
+    # ticks = nus.astype('|S10')
+    # ticks[0] = '1.0=kmeans'
+    plt.xticks(nus, ['1', '2.5', '5', '7.5', '10', '15', '20'])
+    # plt.xticks([0.0, 0.25, 0.5, 0.75, 1.0], ['0.0', '0.25', '0.5', '0.75', '1.0 = k-means'], fontsize=14)
+    # plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0], fontsize=14)
+    plt.grid()
+    plt.xlabel(r'regularization parameter $\nu$', fontsize=14)
+    plt.ylabel(r'Anomaly Detection Accuracy (in AUROC)', fontsize=14)
+    names = list()
+    for i in range(maucs.shape[1]):
+        names.append('ClusterSVDD (k={0})'.format(ks[i]))
+    plt.legend(names, loc=4, fontsize=14)
+    plt.show()
 
-    use_kernels = True
-    if not use_kernels:
-        sigmas = [1.0]
 
-    if plot:
-        if use_kernels:
-            foo = np.load('res_anom_20_7_rbf.npz')
-        else:
-            foo = np.load('res_anom_20_7.npz')
-        maucs = foo['maucs']
-        saucs = foo['saucs']
-        nus = foo['nus']
-        ks = foo['ks']
-
-        plt.figure(1)
-        cols = np.random.rand(maucs.shape[1], 3)
-        fmts = ['-x','--o','--D','--s','--H']
-        for i in range(maucs.shape[1]):
-            plt.errorbar(nus, maucs[:, i], saucs[:, i], fmt=fmts[i], color=cols[i, :], ecolor=cols[i, :], linewidth=2.0, elinewidth=1.0, alpha=0.8)
-        plt.xlim((-0.0, 0.21))
-        plt.ylim((0.35, 1.05))
-        # ticks = nus.astype('|S10')
-        # ticks[0] = '1.0=kmeans'
-        plt.xticks(nus, ['1','2.5','5','7.5','10','15','20'])
-        # plt.xticks([0.0, 0.25, 0.5, 0.75, 1.0], ['0.0', '0.25', '0.5', '0.75', '1.0 = k-means'], fontsize=14)
-        # plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0], fontsize=14)
-        plt.grid()
-        plt.xlabel(r'$\nu$', fontsize=14)
-        plt.ylabel(r'Anomaly Detection Accuracy (in AUROC)', fontsize=14)
-        names = list()
-        for i in range(maucs.shape[1]):
-            names.append('ClusterSVDD (k={0})'.format(ks[i]))
-        plt.legend(names, loc=4, fontsize=14)
-        plt.show()
-        exit(0)
-
+def evaluate(res_filename, nus, sigmas, ks, reps, ntrain, ntest, nval, use_kernels, anom_frac):
     train = np.array(range(ntrain-nval), dtype='i')
     val = np.array(range(ntrain-nval, ntrain), dtype='i')
     test = np.array(range(ntrain, ntrain+ntest), dtype='i')
     aucs = np.zeros((reps, len(nus), len(ks)))
     for n in range(reps):
         # generate new gaussians
-        # data, y = generate_data(ntrain+ntest, outlier_frac=nus[i])
-        data, y = generate_data(ntrain+ntest, outlier_frac=0.05)
+        data, y = generate_data(ntrain+ntest, outlier_frac=anom_frac)
         inds = np.random.permutation(range(ntest+ntrain))
         data = data[:, inds]
         y = y[inds]
@@ -119,7 +102,6 @@ if __name__ == '__main__':
                         max_auc = metrics.auc(fpr, tpr)
                         max_val_auc = curr_auc
                 aucs[n, i, k] = max_auc
-
     # means and standard deviations
     maucs = np.mean(aucs, axis=0)
     saucs = np.std(aucs, axis=0)
@@ -127,12 +109,35 @@ if __name__ == '__main__':
     print np.mean(aucs, axis=0)
     print 'Stds'
     print np.std(aucs, axis=0)
+    # save results
+    np.savez(res_filename, maucs=maucs, saucs=saucs, outlier_frac=nus,
+             ntrain=ntrain, ntest=ntest, reps=reps, nus=nus, ks=ks, sigmas=sigmas)
 
-    if use_kernels:
-        np.savez('res_anom_{0}_{1}_rbf.npz'.format(reps, len(nus)), maucs=maucs, saucs=saucs,
-                outlier_frac=nus, ntrain=ntrain, ntest=ntest, reps=reps, nus=nus, ks=ks, sigmas=sigmas)
-    else:
-        np.savez('res_anom_{0}_{1}.npz'.format(reps, len(nus)), maucs=maucs, saucs=saucs,
-                outlier_frac=nus, ntrain=ntrain, ntest=ntest, reps=reps, nus=nus, ks=ks, sigmas=sigmas)
 
-    print('finished.')
+if __name__ == '__main__':
+    nus = [0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2]
+    sigmas = [0.1, 0.25, 0.5, 1.0, 2.0]
+    ks = [1, 2, 3, 4]
+
+    reps = 20  # number of repetitions for performance measures
+    num_train = 1000  # total number of data points is num_train+num_test
+    num_test = 2000
+    num_val = 400  # num_val is part of ntrain
+    use_kernels = True
+
+    anom_frac = 0.05  # fraction of anomalies in the generated dataset
+
+    do_plot = True
+    do_evaluation = True
+
+    res_filename = 'res_anom_{0}_{1}_{2}_rbf.npz'.format(reps, len(ks), len(nus))
+    if not use_kernels:
+        sigmas = [1.0]
+        res_filename = 'res_anom_{0}_{1}_{2}.npz'.format(reps, len(ks), len(nus))
+
+    if do_evaluation:
+        evaluate(res_filename, nus, sigmas, ks, reps, num_train, num_test, num_val, use_kernels, anom_frac)
+    if do_plot:
+        plot_results(res_filename)
+
+    print('DONE :)')
--- test_robustness.py
+++ test_robustness.py
@@ -26,66 +26,90 @@ def generate_data(datapoints, outlier_frac=0.1, dims=2):
     return X, y
 
 
-if __name__ == '__main__':
-    nus = (np.arange(1, 21)/20.)[::-1]
-    outlier_frac = 0.2  # fraction of uniform noise in the generated data
-    reps = 2  # number of repetitions for performance measures
-    ntrain = 1000
-    ntest = 2000
-    plot = False
+def plot_results(res_filename):
+    foo = np.load(res_filename)
+    maris = foo['maris']
+    saris = foo['saris']
+    nus = foo['nus']
 
-    if plot:
-        foo = np.load('res_10_20.npz')
-        maris = foo['maris']
-        saris = foo['saris']
-        nus = foo['nus']
-        print maris
+    plt.figure(1)
+    cols = np.random.rand(maris.shape[1], 3)
+    fmts = ['-->', '--o', '--D', '--s', '--H']
+    for i in range(maris.shape[1]):
+        plt.errorbar(nus, maris[:, i], saris[:, i], fmt=fmts[i], color=cols[i, :], \
+                     ecolor=cols[i, :], linewidth=2.0, elinewidth=1.0, alpha=0.8)
+    for i in range(maris.shape[1]):
+        plt.errorbar(nus[-1], maris[-1, i], saris[-1, i], \
+                     color='r', ecolor='r', fmt=fmts[i][-1], markersize=6, linewidth=4.0, elinewidth=4.0, alpha=0.7)
 
-        plt.figure(1)
-        plt.errorbar(nus, maris, saris, fmt='.-.b', linewidth=1.0, elinewidth=2.0, alpha=0.8)
-        plt.errorbar(nus[0], maris[0], saris[0], fmt='.-.r', linewidth=1.0, elinewidth=4.0, alpha=1.0)
-        plt.xlim((-0.05, 1.05))
-        plt.ylim((-0.05, 1.05))
-        plt.xticks([0.0, 0.25, 0.5, 0.75, 1.0], ['0.0', '0.25', '0.5', '0.75', '1.0 = k-means'], fontsize=14)
-        plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0], fontsize=14)
-        plt.grid()
-        plt.xlabel(r'regularization parameter $\nu$', fontsize=14)
-        plt.ylabel(r'Adjusted Rand Index (ARI)', fontsize=14)
-        names = ['ClusterSVDD',r'$k$-means']
-        plt.legend(names, loc=1, fontsize=14)
-        plt.show()
+    plt.xlim((-0.05, 1.05))
+    plt.ylim((-0.05, 1.05))
+    plt.xticks([0.0, 0.25, 0.5, 0.75, 1.0], ['0.0', '0.25', '0.5', '0.75', '1.0 = k-means'], fontsize=14)
+    plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0], fontsize=14)
+    plt.grid()
+    plt.xlabel(r'regularization parameter $\nu$', fontsize=14)
+    plt.ylabel(r'Adjusted Rand Index (ARI)', fontsize=14)
+    names = list()
+    for i in range(maris.shape[1]):
+        names.append('ClusterSVDD ($k$={0})'.format(ks[i]))
+    for i in range(maris.shape[1]):
+        names.append('$k$-means ($k$={0})'.format(ks[i]))
+    plt.legend(names, loc=4, fontsize=14)
 
-        exit(0)
+    plt.show()
 
-    train = np.array(range(ntrain), dtype='i')
-    test = np.array(range(ntrain, ntrain+ntest), dtype='i')
 
-    aris = np.zeros((reps, len(nus)))
+def evaluate(res_filename, nus, ks, outlier_frac, reps, num_train, num_test):
+    train = np.array(range(num_train), dtype='i')
+    test = np.array(range(num_train, num_train + num_test), dtype='i')
+
+    aris = np.zeros((reps, len(nus), len(ks)))
     for n in range(reps):
         # generate new gaussians
-        data, y = generate_data(ntrain+ntest, outlier_frac=outlier_frac)
-        inds = np.random.permutation(range(ntest+ntrain))
+        data, y = generate_data(num_train + num_test, outlier_frac=outlier_frac)
+        inds = np.random.permutation(range(num_test + num_train))
         data = data[:, inds]
         y = y[inds]
-        # fix the initialization for all methods
-        membership = np.random.randint(0, 2, y.size)
-
-        for i in range(len(nus)):
-            svdds = [SvddPrimalSGD(nus[i]), SvddPrimalSGD(nus[i])]
-            svdd = ClusterSvdd(svdds)
-            svdd.fit(data[:, train], init_membership=membership[train])
-            _, classes = svdd.predict(data[:, test])
-            # evaluate clustering abilities
-            inds = np.where(y[test] >= 0)[0]
-            # print y[test[inds]]
-            aris[n, i] = metrics.cluster.adjusted_rand_score(y[test[inds]], classes[inds])
-
+        for k in range(len(ks)):
+            # fix the initialization for all methods
+            membership = np.random.randint(0, ks[k], y.size)
+            for i in range(len(nus)):
+                svdds = list()
+                for l in range(ks[k]):
+                    svdds.append(SvddPrimalSGD(nus[i]))
+                svdd = ClusterSvdd(svdds)
+                svdd.fit(data[:, train], init_membership=membership[train])
+                _, classes = svdd.predict(data[:, test])
+                # evaluate clustering abilities
+                inds = np.where(y[test] >= 0)[0]
+                aris[n, i, k] = metrics.cluster.adjusted_rand_score(y[test[inds]], classes[inds])
 
     maris = np.mean(aris, axis=0)
     saris = np.std(aris, axis=0)
     print np.mean(aris, axis=0)
     print np.std(aris, axis=0)
-    np.savez('res_robust_{0}_{1}.npz'.format(reps, len(nus)), maris=maris, saris=saris,
-             outlier_frac=outlier_frac, ntrain=ntrain, ntest=ntest, reps=reps, nus=nus)
+    np.savez(res_filename, maris=maris, saris=saris, outlier_frac=outlier_frac,
+             ntrain=num_train, ntest=num_test, reps=reps, nus=nus)
 
-    print('finished')
+
+if __name__ == '__main__':
+    nus = (np.arange(1, 21)/20.)[::-1]
+    nus = (np.arange(1, 21)/20.)
+    ks = [2, 3, 4]
+
+    outlier_frac = 0.2  # fraction of uniform noise in the generated data
+    reps = 5  # number of repetitions for performance measures
+    num_train = 1000
+    num_test = 2000
+
+    do_plot = True
+    do_evaluation = False
+
+    res_filename = 'res_robust_{0}_{1}_{2}_rbf.npz'.format(reps, len(ks), len(nus))
+
+    if do_evaluation:
+        evaluate(res_filename, nus, ks, outlier_frac, reps, num_train, num_test)
+    if do_plot:
+        plot_results(res_filename)
+
+    print('DONE :)')

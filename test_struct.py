1b11ad379d6ae9ce90a666d3003a0bd7988cebff
nicococo <sinnl0s>
2015-11-22 23:42:17 +0100

    first (messy) final version

--- test_struct.py
+++ test_struct.py
@@ -6,69 +6,61 @@ from svdd_primal_sgd import SvddPrimalSGD
 from cluster_svdd import ClusterSvdd
 
 
-def generate_seqs(lens, block_len, dims=2, proportion=0.5):
-
-    classes = np.random.randint(0,2)
-    seqs = 1.*np.random.randn(dims, lens)*(classes+1.)
+def generate_seqs(lens, block_len, cluster=3, dims=3):
+    classes = np.random.randint(0, cluster)
+    seqs = 1.0*np.random.randn(dims, lens)
     states = np.zeros(lens, dtype='i')
     y = classes
-    # prob = np.random.uniform()
-    # if prob < proportion:
-    #     y = 1
     start = np.random.randint(low=0, high=lens-block_len+1)
     states[start:start+block_len] = 1
-    seqs[:, start:start+block_len] = seqs[:, start:start+block_len]+2.*classes
-
+    # seqs[0, start:start+block_len] = seqs[0, start:start+block_len]+0.5*classes-2.0*float(classes==0)
+    seqs[classes, start:start+block_len] = seqs[0, start:start+block_len]+1.0
     return seqs, states, y
 
 
-def generate_data(datapoints, outlier_frac=0.1, dims=2, plot=True):
+def generate_data(datapoints, cluster=3, outlier_frac=0.1, dims=3, plot=True):
+    lens = 500
     X = []
     S = []
     y = np.ones(datapoints, dtype='i')
-    idx_1 = -1
-    idx_0 = -1
+    idx = np.zeros(cluster, dtype='i')
     idx_anom = -1
     for i in range(datapoints):
-        exm, states, y[i] = generate_seqs(50, 20, dims=dims)
+        exm, states, y[i] = generate_seqs(lens, 250, cluster=cluster, dims=dims)
         prob = np.random.uniform()
         if prob < outlier_frac:
             idx_anom = i
-            exm *= np.random.uniform(low=-0.1,high=+0.1, size=(dims, 50))
+            exm *= np.random.uniform(low=-0.1,high=+0.1, size=(dims, lens))
             exm *= np.exp(10.0*exm)
             y[i] = -1
-        if y[i] == 0:
-            idx_0 = i
-        if y[i] == 1:
-            idx_1 = i
+        else:
+            idx[y[i]] = i
         X.append(exm)
         S.append(states)
 
     if plot:
         plt.figure(1)
-        plt.subplot(1, 3, 1)
-        plt.plot(range(50), X[idx_0][0,:], '-r', alpha=0.7)
-        plt.plot(range(50), S[idx_0], '-b', linewidth=2.0)
-        plt.ylim((-6.5, 6.5))
-        plt.title('Class 0', fontsize=14)
-        plt.xlabel('Sequence index', fontsize=14)
-        plt.ylabel('Feature 0', fontsize=14)
 
-        plt.subplot(1, 3, 2)
-        plt.plot(range(50), X[idx_1][0,:], '-r', alpha=0.7)
-        plt.plot(range(50), S[idx_1], '-b', linewidth=2.0)
-        plt.ylim((-6.5, 6.5))
-        plt.title('Class 1', fontsize=14)
-        plt.xlabel('Sequence index', fontsize=14)
-        plt.ylabel('Feature 0', fontsize=14)
+        for d in range(dims):
+            for i in range(cluster):
+                plt.subplot(1, cluster+1, i+1)
+                plt.plot(range(lens), X[idx[i]][d, :]+d*6., '-r', alpha=0.7)
+                plt.ylim((-5.0, 20.))
+                plt.yticks([0.0])
+                xinds = np.where(S[idx[i]]==1)[0]
+                plt.fill_between(xinds, -5, 20, color=[0.3, 0.3, 0.3], alpha=0.25)
+                plt.title('Class {0}'.format(i), fontsize=14)
+                plt.xlabel('Sequence index', fontsize=14)
+                plt.ylabel('Feature 0  Feature 1  Feature 2', fontsize=14)
 
-        plt.subplot(1, 3, 3)
-        plt.plot(range(50), X[idx_anom][0,:], '-r', alpha=0.7)
-        plt.plot(range(50), S[idx_anom], '-b', linewidth=2.0)
-        plt.ylim((-6.5, 6.5))
+        plt.subplot(1, cluster+1, cluster+1)
+        for d in range(dims):
+            plt.plot(range(lens), X[idx_anom][d, :]+d*6., '-r', alpha=0.7)
+        plt.yticks([0.0])
+        plt.ylim((-5., 20.  ))
         plt.title('Anomalous Data', fontsize=14)
         plt.xlabel('Sequence index', fontsize=14)
-        plt.ylabel('Feature 0', fontsize=14)
+        plt.ylabel('Feature 0  Feature 1  Feature 2', fontsize=14)
 
         plt.show()
     return X, S, y
@@ -114,6 +106,7 @@ def preprocess_test_data(csvdd, X, S, inds):
         scores = csvdd.svdds[k].predict(phis)
         minds = np.where(scores <= min_scores)[0]
         pred_phis[:, minds] = phis[:, minds]
+        min_scores[minds] = scores[minds]
         for i in minds:
             pred_states[i] = states[i]
 
@@ -128,27 +121,6 @@ def hamming_loss(y_true, y_pred):
     return loss / float(N)
 
 
-def calc_emission_matrix(sol, X):
-    T = X.shape[1]
-    N = 2
-    F = X.shape[0]
-    em = np.zeros((N, T))
-    for t in xrange(T):
-        for s in xrange(N):
-            for f in xrange(F):
-                em[s, t] += sol[N*N + s*F + f] * X[f, t]
-    return em
-
-
-def get_transition_matrix(sol):
-    N = 2
-    A = np.zeros((N, N))
-    for i in xrange(N):
-        for j in xrange(N):
-            A[i, j] = sol[i*N+j]
-    return A
-
-
 def argmax(sol, X):
     # if labels are present, then argmax will solve
     # the loss augmented programm
@@ -156,9 +128,18 @@ def argmax(sol, X):
     N = 2
 
     # get transition matrix from current solution
-    A = get_transition_matrix(sol)
+    A = np.zeros((N, N))
+    for i in xrange(N):
+        for j in xrange(N):
+            A[i, j] = sol[i*N+j]
+
     # calc emission matrix from current solution, data points and
-    em = calc_emission_matrix(sol, X)
+    F = X.shape[0]
+    em = np.zeros((N, T))
+    for t in xrange(T):
+        for s in xrange(N):
+            for f in xrange(F):
+                em[s, t] += sol[N*N + s*F + f] * X[f, t]
 
     delta = np.zeros((N, T))
     psi = np.zeros((N, T), dtype='i')
@@ -183,8 +164,8 @@ def argmax(sol, X):
 
 
 def get_joint_feature_map(X, y):
-    T = y.size
     N = 2
+    T = y.size
     F = X.shape[0]
     jfm = np.zeros(N*N + N*F)
     # transition part
@@ -201,39 +182,41 @@ def get_joint_feature_map(X, y):
 
 
 def plot_results(res_filename):
+    data, states, y = generate_data(1000, cluster=3, outlier_frac=0.05, dims=3, plot=False)
+
     foo = np.load(res_filename)
     maris = foo['maris']
     saris = foo['saris']
-    #maris = foo['mloss']
-    #saris = foo['sloss']
+    mloss = foo['mloss']
+    sloss = foo['sloss']
     nus = foo['nus']
     reps = foo['reps']
 
-    plt.figure(1)
-    cols = np.random.rand(maris.shape[1], 3)
-    fmts = ['-->', '--o', '--D', '--s', '--H']
-    for i in range(maris.shape[1]):
-        plt.errorbar(nus, maris[:, i], saris[:, i]/np.sqrt(reps), fmt=fmts[i], color=cols[i, :], \
-                     ecolor=cols[i, :], linewidth=2.0, elinewidth=1.0, alpha=0.8)
-    for i in range(maris.shape[1]):
-        plt.errorbar(nus[-1], maris[-1, i], saris[-1, i]/np.sqrt(reps), \
-                     color='r', ecolor='r', fmt=fmts[i][-1], markersize=6, linewidth=4.0, elinewidth=4.0, alpha=0.7)
+    res = np.zeros((len(nus), 4))
+    res_stds = np.zeros((len(nus), 4))
 
-    plt.xlim((-0.05, 1.05))
-    plt.ylim((-0.05, 1.05))
-    plt.xticks([0.0, 0.25, 0.5, 0.75, 1.0], ['0.0', '0.25', '0.5', '0.75', '1.0 = k-means'], fontsize=14)
-    plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0], fontsize=14)
-    plt.grid()
-    plt.xlabel(r'regularization parameter $\nu$', fontsize=14)
-    plt.ylabel(r'Adjusted Rand Index (ARI)', fontsize=14)
-    names = list()
-    for i in range(maris.shape[1]):
-        names.append('ClusterSVDD ($k$={0})'.format(ks[i]))
-    # for i in range(maris.shape[1]):
-    #     names.append('$k$-means ($k$={0})'.format(ks[i]))
-    plt.legend(names, loc=4, fontsize=14)
+    # svdd
+    res[:, 0] = mloss[:, 0]
+    res_stds[:, 0] = sloss[:, 0]
+    # csvdd
+    res[:, 1] = mloss[:, 1]
+    res_stds[:, 1] = sloss[:, 1]
 
-    plt.show()
+    # kmeans
+    res[0, 2] = maris[0, 1]
+    res_stds[0, 2] = saris[0, 1]
+    # csvdd
+    res[:, 3] = maris[:, 1]
+    res_stds[:, 3] = saris[:, 1]
+
+    print '=========================================='
+    for i in range(len(nus)):
+        line = '{0:1.2f}\\%'.format(nus[i])
+        for j in range(4):
+            line += ' & {0:1.2f}/{1:1.2f}'.format(res[i, j], res_stds[i, j])
+        line += '  \\\\'
+        print line
+    print '=========================================='
 
 
 def evaluate(res_filename, nus, ks, outlier_frac, reps, num_train, num_test):
@@ -244,7 +227,7 @@ def evaluate(res_filename, nus, ks, outlier_frac, reps, num_train, num_test):
     loss = np.zeros((reps, len(nus), len(ks)))
     for n in range(reps):
         # generate new gaussians
-        X, S, y = generate_data(num_train + num_test, outlier_frac=outlier_frac, dims=1, plot=True)
+        X, S, y = generate_data(num_train + num_test, cluster=3, outlier_frac=outlier_frac, dims=3, plot=False)
         inds = np.random.permutation(range(num_test + num_train))
         data = preprocess_training_data(X, S, inds[:num_train])
         data = data[:, inds]
@@ -290,23 +273,23 @@ def evaluate(res_filename, nus, ks, outlier_frac, reps, num_train, num_test):
 if __name__ == '__main__':
     nus = (np.arange(1, 11)/10.)
     ks = [1, 2, 3, 4]
-    nus = [1.0, 0.5, 0.01]
+    nus = [1.0, 0.9, 0.5, 0.1, 0.01]
     ks = [1, 3]
 
-    outlier_frac = 0.0  # fraction of uniform noise in the generated data
-    reps = 1  # number of repetitions for performance measures
-    num_train = 1000
+    outlier_frac = 0.05  # fraction of uniform noise in the generated data
+    reps = 10  # number of repetitions for performance measures
+    num_train = 2000
     num_test = 500
 
-    do_plot = False
-    do_evaluation = True
+    do_plot = True
+    do_evaluation = False
 
     res_filename = 'res_struct_{0}_{1}_{2}.npz'.format(reps, len(ks), len(nus))
 
     if do_evaluation:
         evaluate(res_filename, nus, ks, outlier_frac, reps, num_train, num_test)
     if do_plot:
-        data, states, y = generate_data(num_train + num_test, outlier_frac=outlier_frac, dims=2, plot=True)
+        # data, states, y = generate_data(num_train + num_test, outlier_frac=outlier_frac, dims=2, plot=True)
         plot_results(res_filename)
 
     print('DONE :)')
